9f6cf8973d7814f723b40bdc071c61e988381ccd
superche <shawche@outlook.com>
2021-05-29 00:59:46 +0800

    fix: test issue

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Local outgoing message text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const localMessageElement = screen.getByText(/Local outgoing message/i);
+  expect(localMessageElement).toBeInTheDocument();
 });
